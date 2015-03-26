d2be95097a42d09d5227a7694e8ca973a2602e49
Gabriel Solomon Isko <gsisko@CCCWORK4.WPI.EDU>
2015-03-26 11:40:55 -0400

    fix question 3 binary search

--- Homework_1/Question_3.py
+++ Homework_1/Question_3.py
@@ -3,29 +3,34 @@ from math import floor
 #Question 3
 
 def Binary_Third(K):
-	domain = range(2,200,2)
-	lower = 2
-	upper = 200
-	range = 0
+	domain  = range(2,202,2)
+	lower = 0
+	upper = 100
 	oldsearch = 0
-	search = 0	
+	search = lower + int(floor((upper-lower)/3))
 	comparisons = 0
 
 	while domain[search] != K:
-		oldsearch = search
-		search = floor(lower + ((upper-lower)/3)
-		if domain[search] > K:
+		search = lower + int(floor((upper-lower)/3))
+		print search
+		if oldsearch == search:
+			print 'K was not found in the Domain'
+			break
+		elif domain[search] > K:
 			comparisons += 1
-			lower = search
+			upper = search-1
 		elif domain[search] < K:
 			comparisons += 1
-			upper = search
-		elif K == search:
+			lower = search+1
+		elif K == domain[search]:
 			comparisons += 1
-			print('Found K at index %s after %s comparisons')
-		elif oldsearch == search:
-			print 'K was not found in the Domain'
+			print "Found K at index %s after %s comparisons" % ( search, comparisons)
+			break
+		oldsearch = search
+
 
 #Testing
-Binary_Third(100)
+for i in range(0,200):
+	print "Searching for " + str(i)
+	Binary_Third(i)
 
